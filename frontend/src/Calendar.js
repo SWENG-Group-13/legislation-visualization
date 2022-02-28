a4b67d32dc943cc73fbc785e2a2ab92c3f87374e
Claire Gregg <cgregg@tcd.ie>
2022-02-28 13:21:11 +0000

    Base API call for calendar, with year select

--- frontend/src/Calendar.js
+++ frontend/src/Calendar.js
@@ -1,9 +1,23 @@
+import React from 'react';
+
+
 function Calendar() {
+    getCalendar(2020);
     return (
       <div className="Calendar">
-        
+
       </div>
     );
+  }
+
+  function getCalendar(year){
+    const legislationUrl = 'https://api.oireachtas.ie/v1/legislation?date_start='+year+'-01-01&date_end='+year+'-12-31&limit=100';
+    fetch(legislationUrl)
+      .then(response => response.json())
+      .then(data => {
+        //console.log(data);
+        return data;
+      });
   }
   
   export default Calendar;
