5187c79b63856a35f8c85497b78c3049dad4f622
Jamie Taylor <taylorj8@tcd.ie>
2022-03-02 21:11:00 +0000

    bills from api stored as objects in array

--- frontend/src/Timeline.js
+++ frontend/src/Timeline.js
@@ -1,24 +1,45 @@
 const leg_api = "https://api.oireachtas.ie/v1/legislation"
 
+class Bill
+{
+  constructor(billNo, billType, billYear, status, lastUpdated, longTitleEn)
+  {
+    this.billNo = billNo;
+    this.billType = billType;
+    this.billYear = billYear;
+    this.status = status;
+    this.lastUpdated = lastUpdated;
+    this.longTitleEn = longTitleEn;
+  }
+}
+
 function Timeline() {
-    return (
-      <div className="Timeline">
+  return (
+    <div className="Timeline">
         
-      </div>
-    );
-  }
+    </div>
+  );
+}
 
-  async function getLegislation() {
-    const response = await fetch(leg_api)
-    const legislation = await response.json()
-    const {results: {counts: {billCount, resultCount}}} = data
+async function getLegislation() {
 
-    console.log(billCount)
-    console.log(resultCount)
-  }
+    const response = await fetch(legislation_api)
+    const data = await response.json()
 
-  getLegislation().catch(error => {
-    console.error(error)
+    const {results} = data
+    let bills = []
+    for(let i = 0; i < results.length; i++)
+    {
+      const {bill: {billNo, billType, billYear, status, lastUpdated, longTitleEn}} = results[i];
+      bills[i] = new Bill(billNo, billType, billYear, status, lastUpdated, longTitleEn)
+    }
+
+    console.log(bills)
+    return bills
+}
+
+getLegislation().catch(error => {
+  console.error(error)
 })
 
-  export default Timeline;
+export default Timeline;
