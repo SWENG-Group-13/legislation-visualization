25d169d03135855c66b8c67da4367c5dd900bc44
Owen Gallagher <gallagow@tcd.ie>
2022-03-13 14:32:46 +0000

    added sample form and hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const [data, setData] = React.useState(null);
+  const [data2, setData2] = React.useState("word");
+
+  React.useEffect(() => {
+    setData("hey");
+  }, []);
+
+  const handler = e => {
+    setData2(e.target.value);
+  }
+
   return (
     <div className="App">
-      
+      <h1>{data}</h1>
+      <form>
+        <label>
+          Input?
+          <br></br>
+          <input
+            type="text"
+            value={data2}
+            onChange={handler}
+          ></input>
+        </label>
+      </form>
+      <h2>{data2}</h2>
     </div>
   );
 }
