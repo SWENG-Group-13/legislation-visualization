3d3a485dd20e28fa35d4e9d628f3185dcc5949c4
Owen Gallagher <gallagow@tcd.ie>
2022-03-15 15:11:14 +0000

    prototype of components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,44 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+import Calendar from './Components/Calendar.js';
+import Timeline from './Components/Timeline.js'
 
 function App() {
 
-  const [data, setData] = React.useState(null);
-  const [data2, setData2] = React.useState("word");
+  const [val,setVal] = React.useState([]);
 
-  React.useEffect(() => {
-    setData("hey");
-  }, []);
+  const fetchData = () => {
 
-  const handler = e => {
-    setData2(e.target.value);
+    const url = "https://api.oireachtas.ie/v1/legislation?date_end=2020"
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            const dummy = [
+                // data here
+            ]
+
+            for(let i = 0; i < data.results.length; i++) {
+                let title = data.results[i].bill.shortTitleEn;
+                for(let j = 0; j < data.results[i].bill.stages.length; j++) {
+                    dummy.push({title:title,stage:j+1,date:new Date(data.results[i].bill.stages[j].event.dates[0]["date"])});
+                }
+            }
+
+            setVal(dummy);
+            console.log(dummy);
+        })
   }
-
+  
   return (
     <div className="App">
-      <h1>{data}</h1>
+      <h1>Demonstration</h1>
       <form>
-        <label>
-          Input?
-          <br></br>
-          <input
-            type="text"
-            value={data2}
-            onChange={handler}
-          ></input>
-        </label>
+        <input type="button" value="Fetch" onClick={fetchData}></input>
       </form>
-      <h2>{data2}</h2>
+      <Timeline data={val}/>
+      <Calendar data={val}/>
     </div>
   );
 }
