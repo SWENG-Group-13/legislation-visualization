1250db586e11f3f5cceb3257bc5b9731bffc3389
Claire Gregg <cgregg@tcd.ie>
2022-03-24 11:26:25 +0000

    Added toggle between two graphs and propylon logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Calendar from './Components/Calendar.js'
 import Timeline from './Components/Timeline.js'
+import Toggle from 'react-toggle'
+import "react-toggle/style.css"
 
 function App() {
 
   const fetchData = () => {
 
-    const url = "https://api.oireachtas.ie/v1/legislation?date_end=" + document.getElementById("end_date").value
+    const url = "https://api.oireachtas.ie/v1/legislation?date_end=" + document.getElementById("end_year").value
 
     fetch(url)
         .then(response => response.json())
@@ -31,20 +33,43 @@ function App() {
         })
   }
 
+  const swapGraph = () => {
+    if(graphChosen===0){
+      setGraphChosen(1)
+    } else {
+      setGraphChosen(0)
+    }
+  }
+
   const [val, setVal] = React.useState([]);
+  const [graphChosen, setGraphChosen] = React.useState(0);
 
   return (
     <div className="App">
+      <img src={require('./propylon.jpg')} alt="Propylon Logo" />
       <h1>Demonstration</h1>
+
+      <label>
+        <Toggle 
+          id='graph-selector'
+          onChange={swapGraph}
+          icons={false}
+        />
+        <span>Switch graph view</span>
+      </label>
+      
       <form>
         <label>
-          Enter end date:<br/>
-          <input type="text" id="end_date" size="10" maxLength="4"></input>
+          Enter end year:<br/>
+          <input type="text" id="end_year" size="10" maxLength="4"></input>
         </label>
         <input type="button" value="Fetch" onClick={fetchData}></input>
       </form>
-      <Timeline data={val}/>
-      <Calendar data={val}/>
+      {
+        graphChosen === 0
+        ? <Timeline data={val}/>
+        : <Calendar data={val}/>
+      }
     </div>
   );
 }
